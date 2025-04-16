b340c2d7bbf5b135a7a7ad843c512f60ca36ec98
LobatonXX <202020219@tese.edu.mx>
2025-04-16 16:09:16 -0500

    resposividad completa

--- resources/views/livewire/reset-password.blade.php
+++ resources/views/livewire/reset-password.blade.php
@@ -1,10 +1,9 @@
 <div>
-    <x-header>RECUPERAR CONTRASEÑA</x-header>
+    <x-header>RESTAURAR CONTRASEÑA</x-header>
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
-
         <x-validation-errors class="mb-4" />
 
         <form method="POST" action="{{ route('password.update') }}">
